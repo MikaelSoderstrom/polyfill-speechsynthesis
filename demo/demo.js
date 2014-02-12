23e86ed18380f5f56c2046118dbd58c0790bc331
Mikael Soderstrom <mikael.soderstrom@live.se>
2014-02-12 21:06:46 +0100

    Updated demo

--- demo/demo.js
+++ demo/demo.js
@@ -8,8 +8,6 @@
         speech.lang = document.getElementById('lang').value;
         speech.volume = parseInt(document.getElementById('vol').value) / 100;
         
-        console.log(speech.volume);
-        
         speech.onstart = function() {
             console.log('Starting...');
         };
@@ -19,16 +17,5 @@
         };
         
         window.speechSynthesis.speak(speech);
-        
-        /*
-        var u = new window.SpeechSynthesisUtterance('Bra! Bilden visar en apa, och du svarade ' + answerVal);
-					u.lang = 'sv';
-					u.volume = 1.0;
-					u.rate = 1.0;
-					u.onend = function(event) {
-						console.log('Finished in ' + event.elapsedTime + ' seconds.');
-					};
-					speechSynthesis.speak(u);
-        */
     });
 })();
